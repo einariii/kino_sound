4bc6e3f29c8ddc474604aba358879ebc561b6fca
Einar Engstrom <meengst@gmail.com>
2023-12-12 00:36:02 -0500

    added mix config and license

--- mix.exs
+++ mix.exs
@@ -1,13 +1,25 @@
 defmodule KinoSound.MixProject do
   use Mix.Project
 
+  @name "kino_util"
+  @description "A smart cell for Elixir's Livebook that sonifies your workflow."
+  @version "0.1.0"
+  @source_url "https://github.com/einariii/kino_sound"
+
   def project do
     [
       app: :kino_sound,
+      name: @name,
+      description: @description,
+      source_url: @source_url,
+      version: @version,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :dev,
-      deps: deps()
+      docs: docs(),
+      deps: deps(),
+      aliases: aliases(),
+      package: package()
     ]
   end
 
@@ -18,9 +30,32 @@ defmodule KinoSound.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      extras: [{:"README.md", [title: "Overview"]}],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}"
+    ]
+  end
+
   defp deps do
     [
       {:kino, "~> 0.9.4"}
+    ]
+  end
+
+  defp aliases do
+    [
+      docs: "docs --formatter html"
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/einariii/kino_sound"}
     ]
   end
 end
